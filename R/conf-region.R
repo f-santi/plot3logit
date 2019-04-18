1641eb164e63fc6ed0ff5820b5af6b4d4da967b1
Flavio Santi <flavio.santi@unitn.it>
2019-04-18 17:58:27 +0200

    Overwrite previous commit

--- R/conf-region.R
+++ R/conf-region.R
@@ -1,6 +1,22 @@
 
+#' It computes the confidence region in the ternary space
+#'
+#' Given the parameters of the confidence ellipse in the
+#' space of covariate coefficiets, it returns the confidence
+#' region of the effect.
+#'
+#' @param mu centre of the ellipse.
+#' @param Sig covariance matrix of the ellipse.
+#' @param conf confidence level.
+#' @param npoints number of points of the border.
+#'
+#' @return
+#' `data.frame` with three columns (named `p1`, `p2`, and `p3`)
+#' with ternary coordinates of the 
+#'
+#' @keywords internal
 confregion <- function(mu, Sig, conf = 0.95, npoints = 100) {
-  # Compute the ellipses
+  # Compute the ellipse
   out <- ellipse::ellipse(x = Sig, centre = mu, level = conf)
   out %<>% as.data.frame
   
@@ -10,5 +26,15 @@ confregion <- function(mu, Sig, conf = 0.95, npoints = 100) {
   out$p3 <- 1 / (1 + exp(x) + exp(y))
   
   # Return the confidence ellipse
-  return(out[ , c('p1', 'p2', 'p3')])
+  return(out[ , c('p1', 'p2', 'p3'), drop = FALSE])
 }
+
+
+
+
+
+
+
+
+
+
