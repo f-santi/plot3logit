84542bce818a0ce8adba1c8de6eb49a78b5051a0
Flavio Santi <flavio.santi@univr.it>
2020-04-30 10:11:59 +0200

    Simplified function "confregion"

--- R/conf_region.R
+++ R/conf_region.R
@@ -51,16 +51,13 @@ vcovB2vcovDeltaB <- function(vcovB, vdelta) {
 #' @keywords internal
 confregion <- function(mu, Sig, conf = 0.95, npoints = 100) {
   # Compute the ellipse
-  out <- ellipse::ellipse(x = Sig, centre = mu, level = conf)
-  out %<>% as.data.frame
-  
-  # Compute the ternary coordinates
-  out$p2 <- exp(out$x) / (1 + exp(out$x) + exp(out$y))
-  out$p3 <- exp(out$y) / (1 + exp(out$x) + exp(out$y))
-  out$p1 <- 1 / (1 + exp(out$x) + exp(out$y))
-  
-  # Return the confidence ellipse
-  return(out[ , c('p1', 'p2', 'p3'), drop = FALSE])
+  ellipse::ellipse(x = Sig, centre = mu, level = conf) %>%
+    # Compute the ternary coordinates
+    XB2P_cat3logit %>%
+    # Prepare the output
+    data.frame %>%
+    set_colnames(c('p1', 'p2', 'p3')) %>%
+    return
 }
 
 
