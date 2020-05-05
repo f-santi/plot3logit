b8630b343a641742465a196ccd4ff69832034bc5
Flavio Santi <flavio.santi@univr.it>
2020-05-05 18:05:33 +0200

    Updated help of "cross_1year"

--- R/data.R
+++ R/data.R
@@ -10,21 +10,22 @@
 #'
 #' @format
 #' `data.frame` with 3282 observations of 7 variables:
-#'
-#' * `employment_sit` --- employment situation, a
-#'   `factor` with three levels: *Employed*, *Unemployed*, *Trainee*.
-#' * `gender` --- gender, a `factor` with two levels: *Male*, *Female*.
-#' * `finalgrade` --- final grade degree, a `factor` with three levels:
-#'   *Low*, *Average*, *High*.
-#' * `duration` --- duration of studies, a `factor` with three levels:
-#'   *Short*, *Average*, *Long*.
-#' * `social_class` --- social class, a `factor` with five levels:
+#' \describe{
+#' \item{employment_sit:}{employment situation, a
+#'   factor with three levels: *Employed*, *Unemployed*, *Trainee*.}
+#' \item{gender:}{gender, a factor with two levels: *Male*, *Female*.}
+#' \item{finalgrade:}{final grade degree, a factor with three levels:
+#'   *Low*, *Average*, *High*.}
+#' \item{duration:}{duration of studies, a factor with three levels:
+#'   *Short*, *Average*, *Long*.}
+#' \item{social_class:}{social class, a factor with five levels:
 #'   *Working class*, *White-collar workers*, *Lower middle class*,
-#'   *Upper middle class*, *Unclassified*.
-#' * `irregularity` --- irregularity indicator of student's studies,
-#'   a `factor` with three levels: *Low*, *Average*, *High*.
-#' * `hsscore` --- high school final score, a `numeric` between 60 and
-#'   100.
+#'   *Upper middle class*, *Unclassified*.}
+#' \item{irregularity:}{irregularity indicator of student's studies,
+#'   a factor with three levels: *Low*, *Average*, *High*.}
+#' \item{hsscore:}{high school final score, a numeric between 60 and
+#'   100.}
+#' }
 #'
 #' @references Santi F., M. M. Dickson, G. Espa (2019)
 #' "A graphical tool for interpreting regression coefficients of trinomial logit
