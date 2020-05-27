d29b86bb11fcf8aa803e040f93d8ec43aff9728c
Flavio Santi <flavio.santi@univr.it>
2020-05-27 17:15:46 +0200

    Added dataset "USvote2016"

--- data-raw/USvote2016_prepare.R
+++ data-raw/USvote2016_prepare.R
@@ -9,7 +9,7 @@ read_dta('data-raw/USvote2016.dta') %>%
   # Preliminary variable selection
   select(
     case_identifier, presvote16post_2016, vote_for_against_2016,
-    birthyr_baseline, gender_baseline, race_baseline, educ_baseline,
+    birthyr_baseline, gender_baseline, race_baseline, educ_2016,
     faminc_baseline
   ) %>%
   # Vote against / in favour
@@ -26,7 +26,30 @@ read_dta('data-raw/USvote2016.dta') %>%
     dig.lab = 10, right = FALSE
   )) %>%
   # Education
-  mutate(educ = fct_relevel(educ_baseline, '2-year')) %>%
+  mutate(educ = fct_recode(educ_2016,
+    'No high school' = 'No HS',
+    'High school grad.' = 'High school graduate',
+    'Some college' = 'Some college',
+    '2-year college' = '2-year',
+    '4-year college' = '4-year',
+    'Post-grad' = 'Post-grad',
+    NULL = 'Skipped',
+    NULL = 'Not Asked'
+  )) %>%
+  #mutate(educ = fct_relevel(educ, '2-year college')) %>%
+  # Family income
+  mutate(famincome = fct_collapse(faminc_baseline,
+    '[0; 30,000)' = c('Less than $10,000', '$10,000 - $19,999', '$20,000 - $29,999'),
+    '[30,000; 60,000)' = c('$30,000 - $39,999', '$40,000 - $49,999', '$50,000 - $59,999'),
+    '[60,000; 100,000)' = c('$60,000 - $69,999', '$70,000 - $79,999', '$80,000 - $99,999'),
+    '[100,000; 150,000)' = c('$100,000 - $119,999', '$120,000 - $149,999'),
+    '[150,000; Inf)' = c(
+      '$150,000 or more', '$150,000 - $199,999', '$200,000 - $249,999',
+      '$250,000 - $349,999', '$350,000 - $499,999', '$500,000 or more'
+    ),
+    'NA' = c('Skipped', 'Not Asked', 'Prefer not to say')
+  )) %>%
+  mutate(famincome = fct_recode(famincome, NULL = 'NA')) %>%
   # Race
   mutate(race = fct_lump_min(race_baseline, 100)) %>%
   # Vote
@@ -36,10 +59,7 @@ read_dta('data-raw/USvote2016.dta') %>%
     'Trump' = 'Donald Trump'
   )) %>%
   # Rename variables
-  rename(
-    idcode = case_identifier, famincome = faminc_baseline,
-    gender = gender_baseline
-  ) %>%
+  rename(idcode = case_identifier, gender = gender_baseline) %>%
   # Variable selection
   select(idcode, vote, race, educ, gender, birthyr, famincome) -> USvote2016
 
