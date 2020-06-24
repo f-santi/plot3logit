cfe6fe39a2999f0dc650d304aba0c4a1388b830c
Flavio Santi <flavio.santi@univr.it>
2020-06-24 23:13:30 +0200

    Added the generic "labels<-"

---
+++ R/S3-generics.R
@@ -0,0 +1,20 @@
+
+#' Set the labels of a "field3logit" or a "multifield3logit" object
+#'
+#' It enables the labels of an existing "field3logit" or a "multifield3logit"
+#' object to be set.
+#'
+#' @param x a "field3logit" or a "multifield3logit" object
+#' @param values a character with the new label (or labels in case of a
+#'   "multifield3logit" object)
+#'
+#' @name labels
+#'
+#' @export
+`labels<-` <- function(x, value) {
+  UseMethods(`labels<-`)
+}
+
+
+
+
