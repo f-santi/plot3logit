049715bf84eb28fc37a57fd8057f952a12100976
Flavio Santi <flavio.santi@univr.it>
2020-07-17 16:07:28 +0200

    Documented data "USvote2016"

--- R/data.R
+++ R/data.R
@@ -39,7 +39,12 @@ NULL
 
 #' Self-reported votes from VOTER Survey in 2016
 #'
-#' Self-reported vote survey \insertCite{dfvsg2017;textual}{plot3logit}...
+#' Self-reported votes from 2016 VOTER Survey by
+#' \insertCite{dfvsg2017;textual}{plot3logit}. Object `USvote2016` includes only
+#' few variables based on the result of the survey, which are publicly available
+#' online. See file `"data-raw/USvote2016_prepare.R"` in the GitHub repository
+#' `"f-santi/plot3logit"` \insertCite{santi2020}{plot3logit}, where it is
+#' documented how the dataset `USvote2016` has been generated.
 #'
 #' @name USvote2016
 #' @docType data
@@ -47,13 +52,23 @@ NULL
 #' @format
 #' `tibble` (`data.frame`) with 8000 observations of 7 variables:
 #' \describe{
-#' \item{idcode:}{...}
-#' \item{vote:}{...}
-#' \item{race:}{...}
-#' \item{educ:}{...}
-#' \item{gender:}{gender, a factor with two levels: *Male*, *Female*.}
-#' \item{birthyr:}{...}
-#' \item{famincome:}{...}
+#' \item{idcode:}{voter identifier (integer).}
+#' \item{vote:}{declared vote, afactor with three levels: "Clinton", "Trump",
+#'   "Other".}
+#' \item{race:}{race, a factor with six levels: "White", "Black", "Hispanic",
+#'   "Asian", "Mixed",
+#'   "Other".}
+#' \item{educ:}{level of education, a factor with six levels: "No high school",
+#'   "High school grad.", "Some college", "2-year college", "4-year college",
+#'   "Post-grad".}
+#' \item{gender:}{gender, a factor with four levels: "Male", "Female", "Skipped",
+#'   "Not Asked".}
+#' \item{birthyr:}{decades when the voter was born, a factor with six levels:
+#'   "[1920,1940)", "[1940,1950)", "[1950,1960)", "[1960,1970)", "[1970,1980)",
+#'   "[1980,2000)".}
+#' \item{famincome:}{income (in USD) of voter's family, a factor with five
+#'   levels: "[0; 30,000)", "[30,000; 60,000)", "[60,000; 100,000)",
+#'   "[100,000; 150,000)", "[150,000; Inf)".}
 #' }
 #'
 #' @references
