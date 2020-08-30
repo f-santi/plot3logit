821c27d6ba8a73f9e3805d1686777c6b5c670160
Flavio Santi <flavio.santi@univr.it>
2020-08-30 17:05:37 +0200

    Added test functions for auxiliary test functions

--- R/test-aux.R
+++ R/test-aux.R
@@ -1,6 +1,6 @@
 
 # Generate a random "field3logit" object from matrix
-test_rfield3logit<- function(delta = NULL) {
+test_rfield3logit<- function(delta = NULL, vcov = FALSE) {
   if (is.null(delta)) {
     k <- sample(2:9, 1)
     delta <- plot3logit:::versor(sample(2:k, 1), k)	
@@ -8,10 +8,17 @@ test_rfield3logit<- function(delta = NULL) {
   	k <- length(delta)
   }
   
+  if (is.logical(vcov)) {
+  	if (vcov) {
+  	  matrix(rnorm(4 * k^2), 2 * k, 2 * k) %>%
+  	    crossprod -> vcov
+  	} else { vcov <- NULL }
+  }
+  
   M <- matrix(rnorm(2 * k), k, 2)
   rownames(M) <- paste0('X', 0:(k - 1))
   attr(M, 'levels') <- c('Class A', 'Class B', 'Class C')
   
-  field3logit(M, delta)
+  field3logit(M, delta, vcov = vcov)
 }
 
