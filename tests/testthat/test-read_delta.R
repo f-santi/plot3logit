3db860cbf6001a76eb2ff201dc74385e0276a2b0
Flavio Santi <flavio.santi@univr.it>
2020-09-08 15:42:58 +0200

    Added tests on delta syntax "<<...>>"

--- tests/testthat/test-read_delta.R
+++ tests/testthat/test-read_delta.R
@@ -17,3 +17,45 @@ test_that("function 'get_vdelta' works", {
   expect_identical(depoC, depoN)
 })
 
+
+
+test_that("factor delimiters syntax works", {
+  # Fit the model
+  modVote <- nnet::multinom(
+    formula = vote ~ educ + gender + race + birthyr,
+    data = droplevels(USvote2016),
+    trace = FALSE
+  )
+  
+  
+  # Single factor
+  depo <- field3logit(modVote, '<<gender>>')
+  expect_is(depo + NULL, 'multifield3logit')
+  expect_identical(length(depo + NULL), 1L)
+  
+  depo <- field3logit(modVote, '<<race>>')
+  expect_is(depo, 'multifield3logit')
+  expect_identical(length(depo), 5L)
+  
+  
+  # Two factors
+  depo <- field3logit(modVote, '<<gender>> + <<race>>')
+  expect_is(depo, 'multifield3logit')
+  expect_identical(length(depo), 5L)
+  
+  depo <- field3logit(modVote, '<<race>> + <<race>>')
+  expect_is(depo, 'multifield3logit')
+  expect_identical(length(depo), 25L)
+  
+  
+  # Nested effects
+  list(
+    list(delta = 'genderFemale', label = 'Female'),
+    list(delta = '<<race>>', label = 'Race')
+  ) %>%
+    field3logit(modVote, ., nstreams = 1, narrows = 1) -> depo
+    
+  expect_is(depo, 'multifield3logit')
+  expect_identical(length(depo), 6L)
+})
+
